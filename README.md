522b5a76efdea77b85034c403f6b98c9635616bc
Karthik D <krtdvn@gmail.com>
2021-06-26 14:27:25 +0530

    init

---
+++ README.md
@@ -0,0 +1,124 @@
+# AwesomeRasam
+
+A BeautifulSoup4 wrapper for lazy people. Allows you to extract and clean HTML/XML into neat formats with very few lines of elegant code.
+
+## Installation
+```
+pip3 install awesome-rasam
+```
+
+## Initializing
+
+### From a URL
+AwesomeRasam can use requests and BeautifulSoup4 under the hood to download html from an URL and create a soup object with it
+```
+from awesome_rasam import AwesomeRasam
+
+rasam = AwesomeRasam("https://1upkd.com")
+# or pass in any additional arguments you would pass to requests.get()
+rasam = AwesomeRasam("https://1upkd.com",headers={"User-Agent":"Bot"})
+
+print(rasam.get("title",">text"))
+```
+
+### From Text
+Initialize the soup under-the-hood with HTML/XML formatted text. This is useful when you get HTML through a request sessions or through a headless browser
+```
+from awesome_rasam import AwesomeRasam
+
+html = "<html><head><title>Page Title</title></head><body>Hello</body></html>"
+rasam = AwesomeRasam(html, features="html5lib")
+```
+
+### From a BeautifulSoup4 object
+```
+from awesome_rasam import AwesomeRasam
+from bs4 import BeautifulSoup
+
+html = "<html><head><title>Page Title</title></head><body>Hello</body></html>"
+soup = BeautifulSoup(html, features="html5lib")
+rasam = AwesomeRasam(soup)
+```
+
+## Scraping data
+- All scraping is done by providing CSS selectors to pick elements, and the attributes to pick from those elements.
+- In addition to the attributes present on element tag, special attributes `>text`, `>inner_markup`, `>outer_markup`, and `>rasam`
+- `get()` and `get_all()` methods are provided to select first matching and all matching elements respectively
+- If the element is not found, or the attributed is not present, an Exception is raised. This can be prevented by passing `flag=True`, and optional fallback value can be specified by passing `fallback="N/A"`
+- A `pipe` argument can be passed containing a function or a list of functions to be executed on the result before returning
+```
+import json
+from awesome_rasam import AwesomeRasam
+
+rasam = AwesomeRasam("https://1upkd.com/host-website-on-laptop/")
+blog = {
+    "page_title": rasam.get("title", ">text"),
+    "heading": rasam.get("h1", ">text"),
+    "author": rasam.get(".title p>b", ">text"),
+    "date": rasam.get(".title p>span", ">text", 
+        pipe = lambda x: x.replace("\n","").strip()),
+    "links": rasam.get_all("a","href"),
+    "linked_emails": list(set(rasam.get_all(
+        "a[href^='mailto:']", "href", 
+        pipe = lambda x: x.split("mailto:")[1]))),
+    "linked_emails_are_gmail": rasam.get_all(
+        "a[href^='mailto:']", "href", 
+        pipe = [
+          lambda x: x.split("mailto:")[1],
+          lambda x: x.endswith("@gmail.com")
+        ]),
+    "json_ld_metadata": rasam.get(
+        "script[type='application/ld+json']", ">inner_markup",
+        pipe=json.loads)        
+}
+
+print(json.dumps(blog, indent=2))
+```
+
+### Ultimate flex
+```
+import json
+import random
+
+from awesome_rasam import AwesomeRasam
+
+def parse_blog(rasam):
+    return {
+        "page_title": rasam.get("title", ">text"),
+        "heading": rasam.get("h1", ">text"),
+        "author": rasam.get(".title p>b", ">text"),
+        "date": rasam.get(".title p>span", ">text", 
+            pipe = lambda x: x.replace("\n","").strip()),
+        "links": rasam.get_all("a","href"),
+        "linked_emails": list(set(rasam.get_all(
+            "a[href^='mailto:']", "href", 
+            pipe = lambda x: x.split("mailto:")[1]))),
+        "linked_emails_are_gmail": rasam.get_all(
+            "a[href^='mailto:']", "href", 
+            pipe = [
+              lambda x: x.split("mailto:")[1],
+              lambda x: x.endswith("@gmail.com")
+            ]),
+        "json_ld_metadata": rasam.get(
+            "script[type='application/ld+json']", ">inner_markup",
+            pipe=json.loads)        
+    }
+
+
+
+rasam = AwesomeRasam("https://1upkd.com")
+data = {
+    "page_title": rasam.get("title", ">text"),
+    "blogs": rasam.get_all("#blogs ~ a", "href", pipe=[
+      lambda url: AwesomeRasam(
+          "https://1upkd.com/"+url, 
+          delay=random.randint(1,5)),
+      parse_blog
+    ])        
+}
+
+print(json.dumps(data, indent=2))
+```
+Note: The `delay` argument can be passed while initializing with URL, to delay the request by that many seconds. It can also be a function which returns the number of seconds.
+
+
---
+++ src/awesome_rasam/__init__.py
@@ -0,0 +1,130 @@
+# __init__.py
+
+__version__ = "0.0.5"
+
+import time
+import traceback
+
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+import requests
+
+default_features = "html5lib"
+
+class AwesomeRasam:
+    
+    def __init__(self, initializer, *args, **kwargs):
+        if isinstance(initializer, str):
+            if (initializer.startswith("http://") 
+                    or initializer.startswith("https://")):
+                if 'delay' in kwargs:
+                    if callable(kwargs['delay']):
+                        time.sleep(kwargs['delay']())
+                    else:
+                        time.sleep(kwargs['delay'])
+                    del kwargs['delay']       
+                r = requests.get(initializer, **kwargs)
+                self._soup = BeautifulSoup(r.text, features=default_features)
+            else:
+                self._soup = BeautifulSoup(initializer, *args, **kwargs)
+        elif (isinstance(initializer, BeautifulSoup) 
+              or isinstance(initializer, Tag)):
+            self._soup = initializer
+        else:
+            error_text = "Intializer must be url string, html/xml string, BeautifulSoup or bs4.Element.Tag, but got {} object"
+            error_text = error_text.format(type(initializer))
+            raise UnsupportedInitializer(error_text)    
+                    
+    
+    @staticmethod
+    def _get_attribute(el, selector, attribute, pipe=[], attribute_flag=True, fallback=None):
+        
+        if attribute==">text":
+            extract = el.text
+        elif attribute==">inner_markup":
+            extract = el.decode_contents()
+        elif attribute==">outer_markup":
+            extract = str(el)
+        elif attribute==">rasam":
+            extract = AwesomeRasam(el)    
+        else:
+            extract = el.get(attribute)
+            if extract is None:
+                if attribute_flag:
+                    raise AttributeIsNone("selector={} & attribute={}".format(selector, attribute))
+                else:
+                    return fallback
+        
+        if callable(pipe):
+            pipe = [pipe]
+        
+        for f in pipe:
+            extract = f(extract)    
+                    
+        return extract                                                    
+    
+    def get(self, selector, attribute, pipe=[], flag=True, fallback=None):
+        if selector==">self":
+            el = self._soup
+        else:
+            el_list = self._soup.select(selector)
+            if el_list:
+                el = el_list[0]
+            elif flag:
+                raise ElementNotFound(selector)
+            else:
+                return fallback
+                
+        return AwesomeRasam._get_attribute(el, selector, attribute, pipe, flag, fallback)         
+    
+    def get_all(self, selector, attribute, pipe=[], flag=True, attribute_flag=True, fallback=None):
+        el_list = self._soup.select(selector)
+        if el_list:
+            pass
+        elif flag:
+            raise NoElementsFound(selector)
+        else:
+            return []
+        
+        results = []
+        for el in el_list:
+            results.append(AwesomeRasam._get_attribute(el, selector, attribute, pipe, attribute_flag, fallback))    
+        
+        return results                    
+
+class UnsupportedInitializer(Exception):
+    pass        
+
+class ElementNotFound(Exception):
+    pass
+
+class NoElementsFound(Exception):
+    pass
+
+class AttributeIsNone(Exception):
+    pass
+    
+
+if __name__=="__main__":
+    pass
+    '''url = "https://wios.xyz"
+    print(AwesomeRasam(url, headers={"User-Agent": "Bot"})._soup.title)
+    r = requests.get(url)
+    print(AwesomeRasam(r.text, features="html5lib")._soup.title)
+    print(AwesomeRasam(BeautifulSoup(r.text, features="html5lib"))._soup.title)
+    AwesomeRasam(6, headers={"User-Agent": "Bot"})
+    
+    rasam = AwesomeRasam("https://wios.xyz", delay=1)
+    print(rasam.get("header",">text"))
+    print(rasam.get("header",">inner_markup"))
+    print(rasam.get("header",">outer_markup"))
+    print(rasam.get("header",">rasam").get("a",">rasam").get(">self",">outer_markup"))
+    print(rasam.get("a[href^='https://']","href",pipe = [
+        lambda x: x.split("https://")[1],
+        lambda x: "http://" + x
+    ]))
+    rasam = AwesomeRasam("https://1upkd.com", delay=1)
+    print(rasam.get_all("a[href^='https://']","href",pipe=[
+        lambda x: AwesomeRasam(x, delay=1).get("title",">text",flag=False)
+    ],attribute_flag=False, fallback="lol"))'''
+            
